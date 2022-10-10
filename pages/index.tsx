70ba1447329b9d88f357bb4d7d336be5e5e4446b
ToshiyaEgusa <toshiya.egusa@itcrowd.co.jp>
2022-10-10 12:37:17 +0900

    First Commit

---
+++ pages/index.tsx
@@ -0,0 +1,79 @@
+import Image from 'next/image'
+import Head from 'next/head'
+import { useState } from 'react'
+import { createClient } from '@supabase/supabase-js'
+
+export async function getStaticProps() {
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+  )
+
+  const { data } = await supabaseAdmin
+    .from('mtg-card-list')
+    .select('*')
+    .order('id')
+  return {
+    props: {
+      images: data,
+    },
+  }
+}
+
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+type Image = {
+  id: number
+  href: string
+  imageSrc: string
+  name: string
+  username: string
+}
+
+export default function Gallery({ images }: { images: Image[] }) {
+  return (
+    <div>
+      <h1 className="font-extrabold tracking-tight text-gray-900 text-2xl px-5 py-5">
+        My MTG List
+      </h1>
+
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <Head>
+          <title>My MTG List</title>
+        </Head>
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {images.map((image) => (
+            <BlurImage key={image.id} image={image} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function BlurImage({ image }: { image: Image }) {
+  const [isLoading, setLoading] = useState(true)
+
+  return (
+    <a href={image.href} className="group">
+      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+        <Image
+          alt=""
+          src={image.imageSrc}
+          layout="fill"
+          objectFit="cover"
+          className={cn(
+            'duration-700 ease-in-out group-hover:opacity-75',
+            isLoading
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
+          )}
+          onLoadingComplete={() => setLoading(false)}
+        />
+      </div>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
+    </a>
+  )
+}
